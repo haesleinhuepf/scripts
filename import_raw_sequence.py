9d27290e5938bdd98e44bf81ebb1567dc322db91
haesleinhuepf <rhaase@mpi-cbg.de>
2017-10-02 21:25:33 +0200

    Code cleanup, comments

--- import_raw_sequence.py
+++ import_raw_sequence.py
@@ -1,8 +1,7 @@
 #@File(style="directory") folder
-#@int (value="512") imageWidth
-#@int (value="512") imageHeight
-#@int (value="101") imageDepth
-
+#@int (value=512) imageWidth
+#@int (value=512) imageHeight
+#@int (value=101) imageDepth
 
 from ij import IJ;
 from ij import ImageJ;
@@ -19,6 +18,7 @@ print(folder)
 
 images = [];
 
+# load all images, collect them in a list
 for root, directories, filenames in os.walk(folder.getAbsolutePath()):
     filenames.sort();
     for filename in filenames:
@@ -27,13 +27,16 @@ for root, directories, filenames in os.walk(folder.getAbsolutePath()):
 		    IJ.run("Raw...", "open=[" + os.path.join(folder.getAbsolutePath(), filename) + "] image=[16-bit Signed] width=" + str(imageWidth) + " height=" + str(imageHeight) + " number=" + str(imageDepth) + " little-endian");
 		    imp = IJ.getImage();
 		    images.append(imp);
-		    # imp.hide();
-    
-slices = images[0].getNSlices();
-channels = images[0].getNChannels();
+		    imp.hide();
 
+# merge all images as channels
 merged = RGBStackMerge.mergeChannels(images, False);
 merged.show();
 
-
+# switch channels and frames
 IJ.run(merged,"Re-order Hyperstack ...", "channels=[Frames (t)] slices=[Slices (z)] frames=[Channels (c)]");
+merged = IJ.getImage()
+
+# fix Lookup table
+IJ.run(merged, "Grays", "");
+
