7d92591617e17b09c6329a558775362c03849448
haesleinhuepf <rhaase@mpi-cbg.de>
2018-01-22 12:27:52 +0100

    removed at-parameter stuff because it was buggy

--- import_raw_sequence_resave_as_tif.py
+++ import_raw_sequence_resave_as_tif.py
@@ -5,14 +5,6 @@
 # Author: Robert Haase, http://github.com/haesleinhuepf
 # October 2017
 #
-#
-#@File(style="directory") folder
-#@int (value=512) imageWidth
-#@int (value=512) imageHeight
-#@int (value=101) imageDepth
-#@double (value=0.52) pixelWidth
-#@double (value=0.52) pixelHeight
-#@double (value=2.49) pixelDepth
 
 
 from ij import IJ;
@@ -23,25 +15,38 @@ from ij.plugin import RGBStackMerge;
 from java.io import File;
 from java.util import ArrayList;
 
-
 import os
+
+
+
+
+folder = 'F:/xxwing/2018-01-18-16-30-25-11-Robert_CalibZAP_Wfixed/stacks/default';
+
+imageWidth=512;
+imageHeight=1024;
+imageDepth=111;
+pixelWidth=0.52;
+pixelHeight=0.52;
+pixelDepth=1.981981981981982;
+
+
 
 print(folder)
 
 images = [];
 
 # load all images, collect them in a list
-for root, directories, filenames in os.walk(folder.getAbsolutePath()):
+for root, directories, filenames in os.walk(folder):
     filenames.sort();
     for filename in filenames:
 	    print(filename)
 	    if (filename.endswith(".raw")):
-		    if (not File(os.path.join(folder.getAbsolutePath(), filename + ".tif")).exists()):
-			    IJ.run("Raw...", "open=[" + os.path.join(folder.getAbsolutePath(), filename) + "] image=[16-bit Signed] width=" + str(imageWidth) + " height=" + str(imageHeight) + " number=" + str(imageDepth) + " little-endian");
+		    if (not File(os.path.join(folder, filename + ".tif")).exists()):
+			    IJ.run("Raw...", "open=[" + os.path.join(folder, filename) + "] image=[16-bit Signed] width=" + str(imageWidth) + " height=" + str(imageHeight) + " number=" + str(imageDepth) + " little-endian");
 			    imp = IJ.getImage();
 			    imp.getCalibration().pixelWidth = pixelWidth
 			    imp.getCalibration().pixelHeight = pixelHeight
 			    imp.getCalibration().pixelDepth = pixelDepth
-			    IJ.saveAsTiff(imp, os.path.join(folder.getAbsolutePath(), filename + ".tif") )
+			    IJ.saveAsTiff(imp, os.path.join(folder, filename + ".tif") )
 			    imp.close()
 		    
